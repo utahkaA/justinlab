c6ec170e01febff53936969cf4741bbc94422e8a
utahkaA <utahka.akiba@gmail.com>
2017-03-12 16:52:25 +0900

    create model and create submit page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 end
