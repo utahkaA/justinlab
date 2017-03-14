ff43d54b5b7e9a1caa4b39cf1da27d51c87993d5
utahkaA <utahka.akiba@gmail.com>
2017-03-14 17:40:41 +0900

    clean the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users, :controllers => {
+  #   :registraions => 'users/registraions'
+  # }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tweets#index'
   get 'tweets' => 'tweets#index'
